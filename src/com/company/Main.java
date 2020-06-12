9953dd8994851d251220f6c7bce6c959e1712103
giood <odishvili.giorgi@gmail.com>
2020-06-12 12:26:24 +0400

    Input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception{
+        InputStreamReader inp = new InputStreamReader(new FileInputStream("D:\\read1.txt"));
+        char ch;
+        String s="";
+        while (inp.ready()){
+            s+=(char) inp.read()+"";
+        }
+        inp.close();
+        System.out.println(s);
     }
 }
